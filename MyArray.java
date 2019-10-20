bbeb18eeb827cb4d927fec4da4c778ed1a8a0e91
larren90 <55021927+larren90@users.noreply.github.com>
2019-10-20 20:24:20 +0300

    Update MyArray.java

--- MyArray.java
+++ MyArray.java
@@ -6,41 +6,41 @@ public class MyArray {
     private short[] myArray;
     private int size;
 
-
+//Задается размер массива
     public void Mas() {
         Scanner input = new Scanner(System.in);
         System.out.println("Введите размер массива: ");
         size = input.nextInt();
         myArray =new short[size];
     }
-
+//Заполнение массива с клавиатуры
     public void input(){
         Scanner input = new Scanner(System.in);
         System.out.println("Заполните массив: ");
         for (int i = 0; i < myArray.length; i++)
             myArray[i] = input.nextShort();
     }
-
+//Вывод массива слева-направо
     public void output() {
         System.out.print("Вывод слева-направо: ");
         for (int i = 0; i < myArray.length; i++)
             System.out.print(" " + myArray[i]);
         System.out.print("\n");
     }
-
+//Вывод массива справа-налево
     public void output_inverse() {
         System.out.print("Вывод справа-налево: ");
         for (int i = myArray.length - 1; i >= 0; i--)
             System.out.print(" " + myArray[i]);
         System.out.print("\n");
     }
-
+//Заполнение массива случайными числами
     public void random_Math(int min, int max) {
         for (int i = 0; i < myArray.length; i++) {
             myArray[i] = (short) (Math.random() * max + min);
         }
     }
-
+//Замена максимального элемента массива на минимальный
     public void Change_maxmin(){
         short Max, Min;
         Min = Max = myArray[0];
@@ -58,7 +58,7 @@ public class MyArray {
         myArray[imin] = Max;
         myArray[imax] = Min;
     }
-
+//Удаление элементов массива, содержащих "7"
     public void Delete_7(){
         for (int i = 0; i < myArray.length; i++) {
             String t = Short.toString(myArray[i]);
